6fb75561b9db63d4bbb1c68b360dc262effd5b49
Alex Baden <abaden22@gmail.com>
2021-07-01 07:34:41 -0400

    third update

--- deno-chat-api/index.js
+++ deno-chat-api/index.js
@@ -1,5 +1,5 @@
 addEventListener("fetch", (event) => {
-  const response = new Response("Hello World second try!", {
+  const response = new Response("Hello World third try!", {
     headers: { "content-type": "text/plain" },
   });
   event.respondWith(response);
